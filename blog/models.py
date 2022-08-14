bd8ca3d30f5b2498dacd42e7d994e8484699b207
kholishotula <kholishotula.ka@gmail.com>
2022-08-15 01:24:53 +0700

    feat: data modelling for database

---
+++ blog/models.py
@@ -0,0 +1,10 @@
+from ast import Str
+from .database import Base
+from sqlalchemy import Column, Integer, String
+
+class Blog(Base):
+    __tablename__ = 'blogs'
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    body = Column(String)
