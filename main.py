494803d09d52f0ce50b39e5695349e74310f9eea
kholishotula <kholishotula.ka@gmail.com>
2022-08-15 00:35:35 +0700

    feat: using get route

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
+from typing import Optional
 from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get('/')
-def index():
-    return {'data': {'name': 'Maya'}}
+# @app is path operation decorator
+@app.get('/blog')
+# path operation function
+def index(page = 1, limit = 10, published: Optional[bool] = False):
+    if published:
+        return {'data': f'page {page} with {limit} published blogs from the db'}
+    else:
+        return {'data': f'page {page} with {limit} blogs from the db'}
 
-@app.get('/about')
-def about():
-    return {'data': 'about page'}
+# it should be defined first before the /blog/{id}
+@app.get('/blog/unpublished')
+def unpublished():
+    return {'data': 'all unpublished blog'}
+
+@app.get('/blog/{id}')
+def show(id: int):
+    return {'data': id}
+
+@app.get('/blog/{id}/comments')
+def comments(id: int, page = 1, limit = 10):
+    return {'data': {'1', '2'}}
