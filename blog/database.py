7ecc50a32b19e0b11b4699961139b41b4541be6b
kholishotula <kholishotula.ka@gmail.com>
2022-08-15 01:07:40 +0700

    feat: init db connection

---
+++ blog/database.py
@@ -0,0 +1,15 @@
+from curses import echo
+from enum import auto
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+SQLALCHEMY_DB_URL = 'sqlite:///./blog.db'
+
+engine = create_engine(SQLALCHEMY_DB_URL, connect_args={"check_same_thred": False})
+
+SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+
+Base = declarative_base()
+
+
