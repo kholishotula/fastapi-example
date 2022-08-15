7a9607aeb57a7d27e9fcd06a2f241014128190bb
kholishotula <kholishotula.ka@gmail.com>
2022-08-15 16:29:55 +0700

    feat: get user by id, fix response model for user

--- blog/main.py
+++ blog/main.py
@@ -1,4 +1,5 @@
 import email
+from pyexpat import model
 from typing import List
 from fastapi import FastAPI, Depends, status, HTTPException
 from . import schemas, models
@@ -60,7 +61,7 @@ def destroy(id, db: Session = Depends(get_db)):
     db.commit()
     return {'detail': 'success'}
 
-@app.post('/user')
+@app.post('/user', response_model=schemas.ShowUser)
 def create_user(request: schemas.User, db: Session = Depends(get_db)):
     hashed_pwd = Hash.encrypt(request.password)
     new_user = models.User(name=request.name, email=request.email, password=hashed_pwd)
@@ -68,3 +69,10 @@ def create_user(request: schemas.User, db: Session = Depends(get_db)):
     db.commit()
     db.refresh(new_user)
     return new_user
+
+@app.get("/user/{id}", response_model=schemas.ShowUser)
+def get_user_by_id(id: int, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == id).first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'User with {id} is not available')
+    return user
