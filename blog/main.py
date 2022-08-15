6534dca994bf881edffdca7a2bb9be55d88e9c7c
kholishotula <kholishotula.ka@gmail.com>
2022-08-15 14:52:49 +0700

    feat: update a blog, fix not found when delete

--- blog/main.py
+++ blog/main.py
@@ -22,6 +22,15 @@ def create(request: schemas.Blog, db: Session = Depends(get_db)):
     db.refresh(new_blog)
     return new_blog
 
+@app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED)
+def update(id, request: schemas.Blog, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Blog with {id} is not available')
+    blog.update(request.dict())
+    db.commit()
+    return {'detail': 'success'}
+
 @app.get('/blog', status_code=status.HTTP_200_OK)
 def get_all(db: Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
@@ -41,6 +50,9 @@ def get_by_id(id, db: Session = Depends(get_db)):
 
 @app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT)
 def destroy(id, db: Session = Depends(get_db)):
-    db.query(models.Blog).filter(models.Blog.id == id).delete(synchronize_session=False)
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Blog with {id} is not available')
+    blog.delete(synchronize_session=False)
     db.commit()
-    return {'success'}
+    return {'detail': 'success'}
