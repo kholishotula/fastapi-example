bd70816c4697b457b1c44872902c39b0266f523a
kholishotula <kholishotula.ka@gmail.com>
2022-08-15 23:11:40 +0700

    refactor: use repositories

--- blog/routers/blog.py
+++ blog/routers/blog.py
@@ -1,8 +1,9 @@
-from fastapi import APIRouter, Depends, status, HTTPException
-from .. import schemas, models
+from fastapi import APIRouter, Depends, status
+from .. import schemas
 from ..database import get_db
 from typing import List
 from sqlalchemy.orm import Session
+from ..repository import blog
 
 router = APIRouter(
     prefix="/blog",
@@ -10,42 +11,21 @@ router = APIRouter(
 )
 
 @router.get('/', status_code=status.HTTP_200_OK, response_model=List[schemas.ShowBlog])
-def get_all(db: Session = Depends(get_db)):
-    blogs = db.query(models.Blog).all()
-    return blogs
+def get_all_blog(db: Session = Depends(get_db)):
+    return blog.get_all(db)
 
 @router.post('/', status_code=status.HTTP_201_CREATED)
-def create(request: schemas.Blog, db: Session = Depends(get_db)):
-    new_blog = models.Blog(title=request.title, body=request.body, user_id=1)
-    db.add(new_blog)
-    db.commit()
-    db.refresh(new_blog)
-    return new_blog
+def create_new_blog(request: schemas.Blog, db: Session = Depends(get_db)):
+    return blog.create(request, db)
 
 @router.get('/{id}', status_code=status.HTTP_200_OK, response_model=schemas.ShowBlog)
-def get_by_id(id, db: Session = Depends(get_db)):
-    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
-    if not blog:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail=f'Blog with {id} is not available'
-        )
-    return blog
+def get_blog_by_id(id: int, db: Session = Depends(get_db)):
+    return blog.get_by_id(id, db)
 
 @router.put('/{id}', status_code=status.HTTP_202_ACCEPTED)
-def update(id, request: schemas.Blog, db: Session = Depends(get_db)):
-    blog = db.query(models.Blog).filter(models.Blog.id == id)
-    if not blog.first():
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Blog with {id} is not available')
-    blog.update(request.dict())
-    db.commit()
-    return {'detail': 'success'}
+def update_blog_by_id(id: int, request: schemas.Blog, db: Session = Depends(get_db)):
+    return blog.update(id, request, db)
 
 @router.delete('/{id}', status_code=status.HTTP_204_NO_CONTENT)
-def destroy(id, db: Session = Depends(get_db)):
-    blog = db.query(models.Blog).filter(models.Blog.id == id)
-    if not blog.first():
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Blog with {id} is not available')
-    blog.delete(synchronize_session=False)
-    db.commit()
-    return {'detail': 'success'}
+def delete_blog_by_id(id: int, db: Session = Depends(get_db)):
+    return blog.destroy(id, db)
--- blog/routers/user.py
+++ blog/routers/user.py
@@ -1,9 +1,9 @@
-from fastapi import APIRouter, Depends, HTTPException, status
-from .. import schemas, models, database, hashing
+from fastapi import APIRouter, Depends
+from .. import schemas, database
 from sqlalchemy.orm import Session
+from ..repository import user
 
 get_db = database.get_db
-hash = hashing.Hash
 
 router = APIRouter(
     prefix="/user",
@@ -11,17 +11,10 @@ router = APIRouter(
 )
 
 @router.post('/', response_model=schemas.ShowUser)
-def create_user(request: schemas.User, db: Session = Depends(get_db)):
-    hashed_pwd = hash.encrypt(request.password)
-    new_user = models.User(name=request.name, email=request.email, password=hashed_pwd)
-    db.add(new_user)
-    db.commit()
-    db.refresh(new_user)
-    return new_user
+def create_new_user(request: schemas.User, db: Session = Depends(get_db)):
+    return user.create(request, db)
+    
 
 @router.get("/{id}", response_model=schemas.ShowUser)
 def get_user_by_id(id: int, db: Session = Depends(get_db)):
-    user = db.query(models.User).filter(models.User.id == id).first()
-    if not user:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'User with {id} is not available')
-    return user
+    return user.get_by_id(id, db)
